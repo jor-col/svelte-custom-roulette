b421980fe978409835848c56ed03d2675f0225a0
colin <colin.williams.dev@gmail.com>
2023-06-01 14:56:18 -0500

    wheel container, arrow pointer
    Co-authored-by: codingmustache <contact@codingmustache.dev>

--- src/lib/Wheel.svelte
+++ src/lib/Wheel.svelte
@@ -1,29 +1,44 @@
 <script>
   import Segment from "./Segment.svelte";
-  export let items = ['yes', 'no'];
-  let deg = 360 / items.length  
-
+  export let items = ["yes", "no"];
+  let deg = 360 / items.length;
 </script>
 
-
-<div class='wheel'>
-  {#each items as item, index}
-    <Segment {item} {deg} {index}/>
-  {/each}
+<div class="wheel-container">
+  <div class="wheel">
+    {#each items as item, index}
+      <Segment {item} {deg} {index} />
+    {/each}
+  </div>
+  <img class="pointer" src="src/lib/roulette-pointer.png" alt="pointer" />
 </div>
 
-
-
 <style>
-  .wheel{
-    background: linear-gradient(#e66465, #9198e5);
-    height: 100%s;
-    aspect-ratio: 1/1;
-     clip-path: circle();
-      animation: spin 1.3s linear infinite; 
+  .wheel-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
   }
 
-  
+  .wheel {
+    background: linear-gradient(#e66465, #9198e5);
+    width: 50%;
+    aspect-ratio: 1/1;
+    clip-path: circle();
+    animation: spin 1.3s linear 2;
+  }
+
+  .pointer {
+    position: absolute;
+    top: 50%;
+    left: 48.7%;
+    transform: rotate(135deg);
+    height: 50px;
+    width: 50px;
+  }
+
   @keyframes spin {
     0% {
       transform: rotate(0deg);
@@ -31,5 +46,5 @@
     100% {
       transform: rotate(360deg);
     }
-  } 
+  }
 </style>
