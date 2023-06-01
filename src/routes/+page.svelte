e16840b501f86a53b7680560b4d3ea24172629d8
colin <colin.williams.dev@gmail.com>
2023-06-01 14:08:10 -0500

    JSDoc comments for WheelComponent and Props readme update
    Co-authored-by: codingmustache <contact@codingmustache.dev>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,42 @@
+<script>
+  import { Hello } from "../lib/index.js";
+
+  /**
+   * Represents a customizable roulette wheel component.
+   * @typedef {Object} RouletteWheel
+   * @property {string[]} items - An array of strings to display on the wheel.
+   * @property {string[]} colors - An array of strings to map wheel-segment colors.
+   */
+
+  /**
+   * RouletteWheel component.
+   * @param {Object} options - Options for customizing the roulette wheel.
+   * @param {string[]} [options.items=["yes", "no"]] - The strings to map to wheel-segments.
+   * @param {string[]} [options.colors=["orange", "blue", "purple"]] - An array of strings to map wheel-segment colors.
+   * @returns {RouletteWheel} The created roulette wheel component.
+   */
+
+  /**
+   * `segmentsMap(n)`
+   * @type {(n: string[]) => HTMLElement}
+   * @param {string[]} [n=["yes", "no"]] - takes one argument defaulting to "yes" and "no" if not passed in
+   * @returns {HTMLElement}
+   */
+
+  /**
+   * `colorsMap(n)`
+   * @type {(n: string[]) => void}
+   * @param {string[]} [n=["orange", "blue", "purple"]] - takes one argument array of strings to map segment colors
+   * @returns {void}
+   */
+</script>
+
+<Hello />
 <h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Create your package using @sveltejs/package and preview/showcase your work
+  with SvelteKit
+</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
